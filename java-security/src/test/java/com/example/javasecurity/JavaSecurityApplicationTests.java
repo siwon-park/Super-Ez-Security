b1d921e04dc96e2c797d9a636f5aec9804777655
siwon-park <zow777@naver.com>
2023-04-02 15:57:11 +0900

    initial commit

---
+++ java-security/src/test/java/com/example/javasecurity/JavaSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javasecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
